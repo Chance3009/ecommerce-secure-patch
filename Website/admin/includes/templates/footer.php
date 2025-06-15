dd15705c0babe6c0ccab67d5bcc07df5237a0893
chuxingen0905 <chuxingen@gmail.com>
2025-06-15 09:56:38 +0800

    Upgrade vulnerable JS libraries (Bootstrap, jQuery, jQuery UI)

--- Website/admin/includes/templates/footer.php
+++ Website/admin/includes/templates/footer.php
@@ -10,7 +10,7 @@
             </ul>
         </div>   
     </footer>-->		
-		<script src="<?php echo $js ?>jquery-1.12.1.min.js"></script>
+		<script src="<?php echo $js ?>jquery-3.7.1.min.js"></script>
 		<script src="<?php echo $js ?>jquery-ui.min.js"></script>
 		<script src="<?php echo $js ?>bootstrap.min.js"></script>
 		<script src="<?php echo $js ?>jquery.selectBoxIt.min.js"></script>
